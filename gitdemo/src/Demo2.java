547c14cc096b09ba15cdac16687f182c6edf04bf
SquirrelTXS <65811138+SquirrelTXS@users.noreply.github.com>
2022-07-12 21:11:50 +0800

    master commit6

--- gitdemo/src/Demo2.java
+++ gitdemo/src/Demo2.java
@@ -2,5 +2,6 @@ public class Demo2 {
     public static void main(String[] args) {
         System.out.println("demo2 commit1");
         System.out.println("demo2 commit2");
+        System.out.println("demo2 commit3");
     }
 }
